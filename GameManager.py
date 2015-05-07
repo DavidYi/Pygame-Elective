b7f9e69a6267f7dabfd1c5ae57ebc2c53299808c
David Yi <davidcyyi@gmail.com>
2015-05-07 15:54:25 -0400

    pon

--- GameManager.py
+++ GameManager.py
@@ -6,22 +6,22 @@ from pygame.constants import K_w
 
 pygame.init()
 
-screen=pygame.display.set_mode((640,480),0,32)
+screen = pygame.display.set_mode((640, 480), 0, 32)
 pygame.display.set_caption("Pong Pong!")
 
-#Creating 2 bars, a ball and background.
-back = pygame.Surface((640,480))
+# Creating 2 bars, a ball and background.
+back = pygame.Surface((640, 480))
 background = back.convert()
-background.fill((0,0,0))
-bar = pygame.Surface((10,50))
+background.fill((0, 0, 0))
+bar = pygame.Surface((10, 50))
 bar1 = bar.convert()
-bar1.fill((0,0,255))
+bar1.fill((0, 0, 255))
 bar2 = bar.convert()
-bar2.fill((255,0,0))
-circ_sur = pygame.Surface((15,15))
-circ = pygame.draw.circle(circ_sur,(0,255,0),(15//2,15//2),15//2)
+bar2.fill((255, 0, 0))
+circ_sur = pygame.Surface((15, 15))
+circ = pygame.draw.circle(circ_sur, (0, 255, 0), (15 // 2, 15 // 2), 15 // 2)
 circle = circ_sur.convert()
-circle.set_colorkey((0,0,0))
+circle.set_colorkey((0, 0, 0))
 
 # some definitions
 bar1_x, bar2_x = 10. , 620.
@@ -29,10 +29,10 @@ bar1_y, bar2_y = 215. , 215.
 circle_x, circle_y = 307.5, 232.5
 bar1_move, bar2_move = 0. , 0.
 speed_x, speed_y, speed_circ = 250., 250., 250.
-bar1_score, bar2_score = 0,0
-#clock and font objects
+bar1_score, bar2_score = 0, 0
+# clock and font objects
 clock = pygame.time.Clock()
-font = pygame.font.SysFont("calibri",40)
+font = pygame.font.SysFont("calibri", 40)
 
 while True:
     
@@ -58,17 +58,17 @@ while True:
             elif event.key == K_s:
                 bar1_move = 0.
     
-    score1 = font.render(str(bar1_score), True,(255,255,255))
-    score2 = font.render(str(bar2_score), True,(255,255,255))
+    score1 = font.render(str(bar1_score), True, (255, 255, 255))
+    score2 = font.render(str(bar2_score), True, (255, 255, 255))
 
-    screen.blit(background,(0,0))
-    frame = pygame.draw.rect(screen,(255,255,255),Rect((5,5),(630,470)),2)
-    middle_line = pygame.draw.aaline(screen,(255,255,255),(330,5),(330,475))
-    screen.blit(bar1,(bar1_x,bar1_y))
-    screen.blit(bar2,(bar2_x,bar2_y))
-    screen.blit(circle,(circle_x,circle_y))
-    screen.blit(score1,(250.,210.))
-    screen.blit(score2,(380.,210.))
+    screen.blit(background, (0, 0))
+    frame = pygame.draw.rect(screen, (255, 255, 255), Rect((5, 5), (630, 470)), 2)
+    middle_line = pygame.draw.aaline(screen, (255, 255, 255), (330, 5), (330, 475))
+    screen.blit(bar1, (bar1_x, bar1_y))
+    screen.blit(bar2, (bar2_x, bar2_y))
+    screen.blit(circle, (circle_x, circle_y))
+    screen.blit(score1, (250., 210.))
+    screen.blit(score2, (380., 210.))
 
     bar1_y += bar1_move
     bar2_y += bar2_move
@@ -89,7 +89,7 @@ while True:
     circle_y += speed_y * time_sec
     ai_speed = speed_circ * time_sec
     
-#since i don't know anything about collision, ball hitting bars goes like this.
+# since i don't know anything about collision, ball hitting bars goes like this.
     if circle_x <= bar1_x + 10.:
         if circle_y >= bar1_y - 7.5 and circle_y <= bar1_y + 42.5:
             circle_x = 20.
@@ -101,7 +101,7 @@ while True:
     if circle_x < 5.:
         bar2_score += 1
         circle_x, circle_y = 320., 232.5
-        bar1_y,bar2_y = 215., 215.
+        bar1_y, bar2_y = 215., 215.
     elif circle_x > 620.:
         bar1_score += 1
         circle_x, circle_y = 307.5, 232.5
